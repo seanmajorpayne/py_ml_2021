d38dc701c99d90f180bcc83d56d4fb8276eb97c2
seanmajorpayne <seanmajorpayne@gmail.com>
2021-08-25 16:42:42 +0900

    adding helper functions for data loading

--- utils.py
+++ utils.py
@@ -2,6 +2,56 @@ import numpy as np
 import pandas as pd
 from sklearn.utils import shuffle
 
+def get_data():
+    """
+    Loads the MNIST dataset and separates it into an train and validation sets.
+    :return: Train/Validation Input Numpy Matrices & Output Vectors
+    """
+    df = pd.read_csv("train.csv")
+    X_Y = df.to_numpy().astype(np.float32)
+    Xtrain = X_Y[:-1000, 1:]
+    Xtest = X_Y[-1000:, 1:]
+    Ytrain = X_Y[:-1000, 0]
+    Ytest = X_Y[-1000:, 0]
+    return Xtrain, Xtest, Ytrain, Ytest
+
+
+def get_binary_data(X, Y):
+    """
+    Returns only label classes 0 and 1 for a given input matrix and output vector.
+    :return: Numpy Matrix, Numpy Vector
+    """
+    X = X[Y <= 1]
+    Y = Y[Y <= 1]
+    return X, Y
+
+
+def normalize_data(Xtrain, Xtest):
+    """
+    Normalizes Numpy Matrix Data for train and validation sets.
+    :param Xtrain: Numpy Matrix of Inputs
+    :param Xtest: Numpy Vector of Outputs
+    """
+    mu = Xtrain.mean(axis=0)
+    std = Xtrain.std(axis=0)
+    np.place(std, std == 0, 1)
+    Xtrain = (Xtrain - mu) / std
+    Xtest = (Xtest - mu) / std
+    return Xtrain, Xtest
+
+
+def create_indicator_matrix(Y, K):
+    """
+    Takes a vector and one-hot encodes it into an indicator matrix.
+    Used to compare with predictions since shape will be different.
+    :param Y: Vector of Outputs
+    :return: Numpy Matrix
+    """
+    Y_ind = np.zeros(len(Y), K)
+    for i in range(len(Y)):
+        Y_ind[i, Y[i]] = 1
+    return Y_ind
+
 
 def sigmoid(Z):
     """
@@ -128,4 +178,14 @@ def gradient_b1(Z, Y, T, W2):
 def cross_entropy_cost(Y, T):
     total = T * np.log(Y)
     return total.sum()
+
+
+def classification_rate(Y, T):
+    """
+    Calculates the classification rate of predictions given targets.
+    :param Y: Vector of Predictions
+    :param T: Vector of Targets
+    :return: Score as float
+    """
+    return np.mean(Y == T)
 
