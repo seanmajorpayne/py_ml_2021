9f1f43e839261e6e2c16ae2395559d46e10e7f49
seanmajorpayne <seanmajorpayne@gmail.com>
2021-08-27 16:21:32 +0900

    adding linear regression

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -7,25 +7,24 @@ class LinearRegression(Model):
     def __init__(self):
         super().__init__()
 
-    def fit(self, x_train, y_train, validation_data, epochs=1000, learning_rate=0.01):
+    def fit(self, x_train, y_train, validation_data, epochs=100, learning_rate=0.00000001):
         x_test, y_test = validation_data
-        _, D = x_train.shape
+        N, D = x_train.shape
         self.W = np.random.randn(D) / np.sqrt(D)
 
         train_costs = []
         test_costs = []
+        l2 = 10
         for i in range(epochs):
-            p_y_train = x_train.dot(self.W) + self.b
+            p_y_train = x_train.dot(self.W)
             train_cost = self.cost(p_y_train, y_train)
             train_costs.append(train_cost)
 
-            p_y_test = x_test.dot(self.W) + self.b
+            p_y_test = x_test.dot(self.W)
             test_cost = self.cost(p_y_test, y_test)
             test_costs.append(test_cost)
 
-            self.W -= learning_rate * x_train.T.dot(p_y_train - y_train)
+            if i % 10 == 0:
+                print(i, train_cost, test_cost)
 
-        self.history["train_costs"] = train_costs
-        self.history["test_costs"] = test_costs
-        self.predictions["Training Predictions"] = p_y_train
-        self.predictions["Test Predictions"] = p_y_test
+            self.W -= learning_rate * (x_train.T.dot(p_y_train - y_train))
